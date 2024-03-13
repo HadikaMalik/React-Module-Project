19c9b63a5d6e4630974a3db0797497ab0cf33b62
Pedro Ricciardi <riccip@outlook.com>
2024-03-13 08:27:00 +0000

    customer profile data fetch implemented, some css changed

--- src/components/CustomerProfile/CustomerProfile.jsx
+++ src/components/CustomerProfile/CustomerProfile.jsx
@@ -1,8 +1,32 @@
+import { useState, useEffect } from "react";
+
 import "./CustomerProfile.scss";
 
-const CustomerProfile = (props) => {
-  const { id } = props;
-  return <tr data-testid="customer-profile">Customer {id} profile</tr>;
+const CustomerProfile = ({ id }) => {
+  const [customerProfileData, setCustomerProfileData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://cyf-hotel-api.netlify.app/customers/${id}`)
+      .then((response) => response.json())
+      .then((data) => setCustomerProfileData(data))
+      .catch((error) =>
+        console.error("Error fetching customer profile:", error)
+      );
+  }, [id]);
+
+  if (!customerProfileData) {
+    return (
+      <tr data-testid="customer-profile" className="customer-row">
+        <td colSpan={2}>Loading...</td>
+      </tr>
+    );
+  }
+
+  return (
+    <tr data-testid="customer-profile" className="customer-row">
+      <td colSpan={2}>{customerProfileData.phoneNumber}</td>
+    </tr>
+  );
 };
 
 export default CustomerProfile;
