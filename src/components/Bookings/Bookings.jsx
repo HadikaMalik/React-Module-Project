0fa0603ec9b045b198fd6e7eca5265a5cd092fdf
Pedro Ricciardi <riccip@outlook.com>
2024-03-14 09:32:00 +0000

    fetching bookings from api

--- src/components/Bookings/Bookings.jsx
+++ src/components/Bookings/Bookings.jsx
@@ -1,15 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Search from "@/components/Search/Search";
 import FakeBookings from "@/data/fakeBookings.json";
 import SearchResults from "@/components/SearchResults/SearchResults";
 
 const Bookings = () => {
+  const API = "https://cyf-hotel-api.netlify.app/";
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => setBookings(data));
+  }, []);
+
   const search = (searchVal) => {
     console.info("TO DO!", searchVal);
   };
-
-  const [bookings, setBookings] = useState(FakeBookings);
 
   return (
     <main className="bookings">
