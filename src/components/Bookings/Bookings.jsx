162a276a443ad2aa36c9e60cd604ac0180c9650a
Pedro Ricciardi <riccip@outlook.com>
2024-03-14 10:02:58 +0000

    catch error implemented

--- src/components/Bookings/Bookings.jsx
+++ src/components/Bookings/Bookings.jsx
@@ -1,23 +1,37 @@
 import { useState, useEffect } from "react";
-
 import Search from "@/components/Search/Search";
-import FakeBookings from "@/data/fakeBookings.json";
 import SearchResults from "@/components/SearchResults/SearchResults";
+
+import "./Bookings.scss";
 
 const Bookings = () => {
   const API = "https://cyf-hotel-api.netlify.app/";
   const [bookings, setBookings] = useState([]);
+  const [fetchError, setFetchError] = useState(null);
 
   useEffect(() => {
     fetch(API)
       .then((response) => response.json())
-      .then((data) => setBookings(data));
+      .then((data) => setBookings(data))
+      .catch((error) => {
+        setFetchError(error);
+        console.log(error);
+      });
   }, []);
 
   const search = (searchVal) => {
     console.info("TO DO!", searchVal);
   };
 
+  if (fetchError) {
+    return (
+      <main id="bookings-fetch-error-message">
+        <h2>There was an error fetching the data</h2>
+        <h3>{fetchError.name}</h3>
+        <p>{fetchError.message}</p>
+      </main>
+    );
+  }
   return (
     <main className="bookings">
       <Search search={search} />
