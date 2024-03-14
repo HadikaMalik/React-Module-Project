2f24061e38caa9cd3cd323bbfb00de2b29f2ba3c
Pedro Ricciardi <riccip@outlook.com>
2024-03-14 10:24:15 +0000

    error managment improved

--- src/components/Bookings/Bookings.jsx
+++ src/components/Bookings/Bookings.jsx
@@ -1,17 +1,25 @@
 import { useState, useEffect } from "react";
+import { getReasonPhrase } from "http-status-codes";
 import Search from "@/components/Search/Search";
 import SearchResults from "@/components/SearchResults/SearchResults";
 
 import "./Bookings.scss";
 
 const Bookings = () => {
-  const API = "https://cyf-hotel-api.netlify.app/";
+  const API = "https://cyf-hotel-api.netlify.app/error";
   const [bookings, setBookings] = useState([]);
   const [fetchError, setFetchError] = useState(null);
 
   useEffect(() => {
     fetch(API)
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `${response.status}: ${getReasonPhrase(response.status)}`
+          );
+        }
+        return response.json();
+      })
       .then((data) => setBookings(data))
       .catch((error) => {
         setFetchError(error);
@@ -27,12 +35,12 @@ const Bookings = () => {
     return (
       <main id="bookings-fetch-error-message">
         <h2>There was an error fetching the data</h2>
-        <h3>{fetchError.name}</h3>
+        <h3>Description</h3>
         <p>{fetchError.message}</p>
       </main>
     );
   }
-  
+
   return (
     <main className="bookings">
       <Search search={search} />
